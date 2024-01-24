84d554395f3a392e96afe682412ccbdf8bfe2f76
Dainichi1 <96421043+Dainichi1@users.noreply.github.com>
2024-01-24 14:20:15 +0100

    Add files via upload

---
+++ com/SEZIONE_4_33_Hello_world_in_Intellij/Hello.java
@@ -0,0 +1,10 @@
+package it.udemy.com.SEZIONE_4_33_Hello_world_in_Intellij;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+        System.out.println("Hello World!");
+    }
+
+}
