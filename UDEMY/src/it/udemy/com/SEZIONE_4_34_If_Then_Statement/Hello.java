03d444d08270525e466c8b71cb6b5ff4a9ef1d6a
Dainichi1 <96421043+Dainichi1@users.noreply.github.com>
2024-01-24 14:20:49 +0100

    Add files via upload

---
+++ UDEMY/src/it/udemy/com/SEZIONE_4_34_If_Then_Statement/Hello.java
@@ -0,0 +1,15 @@
+package it.udemy.com.SEZIONE_4_34_If_Then_Statement;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+    }
+
+}
