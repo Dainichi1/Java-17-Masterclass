9c6eb0117af8568a84cc4c0696e070f9d3c95b69
Dainichi1 <96421043+Dainichi1@users.noreply.github.com>
2024-01-24 14:27:17 +0100

    Delete UDEMY directory

--- UDEMY/src/it/udemy/com/SEZIONE_4_32_Hello_world_in_Intellij/FirstClass.java
+++
@@ -1,7 +0,0 @@
-package it.udemy.com.SEZIONE_4_32_Hello_world_in_Intellij;
-
-public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
