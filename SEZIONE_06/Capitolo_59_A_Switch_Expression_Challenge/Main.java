129e99c1eded4083058007e5c204623de68580fb
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-02-07 10:08:59 +0100

    Aggiunto Capitolo 59

---
+++ SEZIONE_06/Capitolo_59_A_Switch_Expression_Challenge/Main.java
@@ -0,0 +1,30 @@
+package SEZIONE_06.Capitolo_59_A_Switch_Expression_Challenge;
+
+import java.time.MonthDay;
+
+public class Main {
+    public static void main(String[] args) {
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+    }
+
+    public static void printDayOfWeek (int day) {
+        String dayOfTheWeek =  switch (day) {
+            case 0 -> { yield "Monday"; }
+            case 1 -> "Tuesday";
+            case 2 -> "Wednesday";
+            case 3 -> "Thursday";
+            case 4 -> "Friday";
+            case 5 -> "Saturday";
+            case 6 -> "Sunday";
+            default -> "Invalid Day";
+            };
+        System.out.println(day + " stands for: "+ dayOfTheWeek);
+        }
+    }
