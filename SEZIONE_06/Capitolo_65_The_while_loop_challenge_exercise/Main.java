26b96b696dd9f6223052380613c11d1fd6afffc2
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-02-12 19:07:39 +0100

    Aggiunta "Lezione 65"

---
+++ SEZIONE_06/Capitolo_65_The_while_loop_challenge_exercise/Main.java
@@ -0,0 +1,23 @@
+package SEZIONE_06.Capitolo_65_The_while_loop_challenge_exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 4;
+        while (i <= 20) {
+            i++;
+            if (isEvenNumber(i)) {
+                if (sum ==  5) {
+                    break;
+                }
+                sum ++;
+                System.out.println("Even number: "+i);
+            }
+        }
+        System.out.println("Total even number: "+sum);
+    }
+
+    public static boolean isEvenNumber (int number) {
+        return number % 2 == 0;
+    }
+}
