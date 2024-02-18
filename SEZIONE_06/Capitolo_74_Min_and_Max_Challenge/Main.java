5fde86838a989ad96162bc6d4443099d6b3d2f3e
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-02-18 12:16:57 +0100

    Aggiunto "Capitolo 74"

---
+++ SEZIONE_06/Capitolo_74_Min_and_Max_Challenge/Main.java
@@ -0,0 +1,42 @@
+package SEZIONE_06.Capitolo_74_Min_and_Max_Challenge;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int min = 0;
+        int max = 0;
+        int loop = 0;
+
+
+        while (true) {
+            System.out.println("Inserisci un numero: ");
+            String nextInt = scanner.nextLine();
+            try {
+
+                int number = Integer.parseInt(nextInt);
+
+                if (number < min || loop==0) {
+                    min = number;
+                }
+
+                if (number > max || loop == 0) {
+                    max = number;
+                }
+                loop++;
+                System.out.println("Il minimo è: "+min+" e il massimo è: "+max);
+
+            } catch (NumberFormatException nfe) {
+                break;
+            }
+        }
+        if (loop > 0) {
+            System.out.println("min = "+min+", max = "+max);
+        } else {
+            System.out.println("Inserimento non valido");
+        }
+    }
+}
+
