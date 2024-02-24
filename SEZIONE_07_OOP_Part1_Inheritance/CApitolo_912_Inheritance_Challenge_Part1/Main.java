cc31b2c191052345b5a139c0772c88787d0808cc
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-02-24 15:12:03 +0100

    Aggiunto "Capitolo 92"

---
+++ SEZIONE_07_OOP_Part1_Inheritance/CApitolo_912_Inheritance_Challenge_Part1/Main.java
@@ -0,0 +1,15 @@
+package SEZIONE_07_OOP_Part1_Inheritance.CApitolo_912_Inheritance_Challenge_Part1;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee tim = new Employee("Tim","11/11/1985",
+                "01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age = "+tim.getAge());
+        System.out.println("Pay = "+tim.collectPay());
+
+        Employee joe = new Employee("Joe","11/11/1990",
+                "03/03/2020");
+        System.out.println(joe);
+    }
+}
---
+++ SEZIONE_07_OOP_Part1_Inheritance/CApitolo_912_Inheritance_Challenge_Part1/Worker.java
@@ -0,0 +1,45 @@
+package SEZIONE_07_OOP_Part1_Inheritance.CApitolo_912_Inheritance_Challenge_Part1;
+
+
+
+public class Worker {
+
+    private String name;
+    private String birthDate;
+    protected String endDate;
+
+    public Worker () {
+
+    }
+
+
+    public Worker(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+
+        int currentYear = 2025;
+        int birthYear = Integer.parseInt(birthDate.substring(6)); // 09-12-1979 con substrin (6) elimina i primi 6 caratteri (09-12-)
+
+        return (currentYear - birthYear);
+    }
+
+    public double collectPay() {
+        return 0.0;
+    }
+
+    public void terminate (String endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
