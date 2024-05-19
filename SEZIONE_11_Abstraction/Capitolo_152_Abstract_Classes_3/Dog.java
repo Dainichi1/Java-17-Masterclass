8a4b13a5c8af535f3a4fa339503b27ef100681ba
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-05-19 17:56:04 +0200

    Aggiunto "Capitolo 153"

---
+++ SEZIONE_11_Abstraction/Capitolo_152_Abstract_Classes_3/Dog.java
@@ -0,0 +1,38 @@
+package SEZIONE_11_Abstraction.Capitolo_152_Abstract_Classes_3;
+
+
+public class Dog extends Mammal {
+
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+        if (speed.equals("slow")) {
+            System.out.println(getExplicitType() + " walking");
+        } else {
+            System.out.println(getExplicitType() + " running");
+        }
+    }
+
+    @Override
+    public void shedHair() {
+
+        System.out.println(getExplicitType()+ " shed hair all the time");
+    }
+
+    @Override
+    public void makeNoise() {
+
+        if (type == "Wolf") {
+            System.out.print("Howling! ");
+        } else {
+            System.out.print("Woof! ");
+        }
+    }
+}
+
+
