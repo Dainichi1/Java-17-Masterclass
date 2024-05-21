abb7b182a022a946705c7eba132de740589f3129
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-05-21 18:18:04 +0200

    Aggiunto "Capitolo 161 e 162"

---
+++ SEZIONE_11_Abstraction/Capitolo_161_162_Interface_Challenge_1_2/Main.java
@@ -0,0 +1,25 @@
+package SEZIONE_11_Abstraction.Capitolo_161_162_Interface_Challenge_1_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Mappable> mappables = new ArrayList<>();
+
+
+        mappables.add(new Building("Sydney Town Hall", UsageType.GOVERNMENT));
+        mappables.add(new Building("Sydney Opera House",
+                UsageType.ENTERTAINMENT));
+        mappables.add(new Building("Stadium Australia", UsageType.SPORTS));
+
+        mappables.add(new UtilityLine("College St",UtilityType.FIBER_OPTIC));
+        mappables.add(new UtilityLine("Olympic Blvd",UtilityType.WATER));
+
+        for (var m : mappables) {
+            Mappable.mapIt(m);
+        }
+    }
+}
