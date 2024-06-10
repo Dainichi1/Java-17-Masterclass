667df1568350aedfa0c08ee66404e2dec6a89177
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-10 12:54:47 +0200

    Aggiunto "Capitolo 173"

---
+++ SEZIONE_11_Generics/Capitolo_173_Static_methods_and_multiple_upper_bounds/QueryList.java
@@ -0,0 +1,35 @@
+package SEZIONE_11_Generics.Capitolo_173_Static_methods_and_multiple_upper_bounds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryList <T extends QueryItem> {
+
+    private List<T> items;
+
+    public QueryList(List<T> items) {
+        this.items = items;
+    }
+
+    public static <T extends QueryItem> List<T> getMatches (List<T> items, String field, String value) {
+
+        List<T> matches = new ArrayList<>();
+        for (var item : items) {
+            if (item.matchFieldValue(field,value)) {
+                matches.add(item);
+            }
+        }
+        return matches;
+    }
+
+    public List<T> getMatches (String field, String value) {
+
+        List<T> matches = new ArrayList<>();
+        for (var item : items) {
+            if (item.matchFieldValue(field,value)) {
+                matches.add(item);
+            }
+        }
+        return matches;
+    }
+}
