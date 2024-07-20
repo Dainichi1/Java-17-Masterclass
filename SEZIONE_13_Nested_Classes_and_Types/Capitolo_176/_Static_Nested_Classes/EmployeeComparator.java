f02770da0ba388789db8c65d133d1434859876ec
Dainichi1 <torquati79@yahoo.it>
2024-07-20 09:22:53 +0200

    Aggiunto "Capitolo 176"

---
+++ SEZIONE_13_Nested_Classes_and_Types/Capitolo_176/_Static_Nested_Classes/EmployeeComparator.java
@@ -0,0 +1,11 @@
+package SEZIONE_13_Nested_Classes_and_Types.Capitolo_176._Static_Nested_Classes;
+
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
--- SEZIONE_13_Nested_Classes_and_Types/Capitolo_176/_Static_Nested_Classes/Main.java
+++ SEZIONE_13_Nested_Classes_and_Types/Capitolo_176/_Static_Nested_Classes/Main.java
@@ -1,4 +1,30 @@
 package SEZIONE_13_Nested_Classes_and_Types.Capitolo_176._Static_Nested_Classes;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee(10001, "Ralph", 2015),
+                new Employee(10005, "Carole", 2021),
+                new Employee(10022, "Jane", 2013),
+                new Employee(13151, "Laura", 2020),
+                new Employee(10050, "Jim", 2018) ));
+
+//        var comparator = new EmployeeComparator<>();
+//        employees.sort(comparator);
+
+        employees.sort(new Employee.EmployeeComparator<>("yearStarted")
+                .reversed());
+
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+
+    }
 }
